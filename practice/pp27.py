261a82607109b5c490fece19e9b0c452265d2c8a
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-04 14:49:30 +0530

    Updated project files

--- practice/pp27.py
+++ practice/pp27.py
@@ -4,3 +4,16 @@ for i in range(5):
     a=int(input("Enter the value: "))
     l.append(a)
 print(l)
+
+#WAP TO COLLECT FIVE INT VALUE FROM USER AND PRINT ONLY EVEN NUMBERS FROM ELEMENT
+l=[]
+e=[]
+n=input("enter the range :")
+for i in range(int(n)):
+    a=int(input("Enter the value:"))
+    l.append(a)
+for i in l:
+    if i%2==0:
+        e.append(i)
+print(l)
+print(e)
