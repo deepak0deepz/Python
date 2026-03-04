633cd9e6d049a7f3de4ed8495ae91c43279805d1
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-04 14:06:07 +0530

    Updated project files

--- practice/pp26.py
+++ practice/pp26.py
@@ -1,16 +1,18 @@
-a=10
+
 def fn():
+    global a
     a=100
     print(a)
     b=20
     print(b)
 def fn1():
+    global a
     a=1000
     print(a)
     c=30
     print(c)
-print(a)
+
 fn()
 print(a)
-fn1()
+fn1() 
 print(a)
