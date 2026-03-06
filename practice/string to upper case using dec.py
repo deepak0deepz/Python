103badf6576b3c89d501871aa1951b2936d03112
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-06 14:34:24 +0530

    Updated project files

--- practice/string to upper case using dec.py
+++ practice/string to upper case using dec.py
@@ -1,1 +1,17 @@
 #WAP to  convert from string to upper case by using decoratore in python
+def print_msg():
+    msg="deepak"
+    return msg
+def outer(print1):
+    print("entering outer")
+    def inner():
+        print("inside inner")
+        ref=print1
+        res=ref()
+        res1=res.upper()
+        print(res1)
+        print("leaving inner")
+    return inner
+ptr1=outer(print_msg)
+ptr1()
+print("pgm end")
