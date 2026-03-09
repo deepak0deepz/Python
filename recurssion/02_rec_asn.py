95e942a6ffc15e2be2d49c000a83ab1780d3df24
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-09 10:33:26 +0530

    Updated project files

--- recurssion/02_rec_asn.py
+++ recurssion/02_rec_asn.py
@@ -1,3 +1,11 @@
+def reccountnum(n,count=0):
+    if n<=0:
+        return count
+    n//=10
+    count+=1
+    return reccountnum(n,count)
+n=int(input("enter the number to check count of it"))
+print(f"the count of {n} is",reccountnum(n))
 def rec_asn(n,power,temp,asn=0):
     if n<=0:
         return asn==temp
