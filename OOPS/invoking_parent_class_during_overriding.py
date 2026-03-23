d38627cf09cd8ae06e6c8f1f892f2d81cd373ab8
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-23 14:03:40 +0530

    Updated project files

---
+++ OOPS/invoking_parent_class_during_overriding.py
@@ -0,0 +1,19 @@
+class A:
+    def __init__(self):
+        self.a=10
+    def display(self):
+        print("Inside A")
+class B(A):
+    def display(self):
+        print("Inside B")
+class C(B):
+    def display(self):
+        print("Inside C")
+class D(C):
+    def dispD(self):
+        B.display(self)
+        A.display(self)
+        C.display(self)
+d1=D()
+d1.dispD()
+
