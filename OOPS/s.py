732f3466a6014c06a4dd7024a446d528b63001be
deepak0deepz <deepakcs2k4@gmail.com>
2026-03-23 13:16:19 +0530

    Updated project files

--- OOPS/s.py
+++ OOPS/s.py
@@ -1,38 +1,12 @@
-class Heart:
-    def __init__(self, name):
-        self.hname = name
-        print(f"{self.hname} heart is created")
-
-    def getheart(self):
-        print("Heart is working")
-
-class Car:
-    def __init__(self, name):
-        self.cname = name
-        print("Car is ready")
-
-    def getCar(self):
-        print("Car is running")
-
-class Person:
-    def __init__(self, name):
-        self.pname = name
-        self.heart = Heart("Human")
-        self.car = None
-        
-        print("Person is created")
-
-    def hasCar(self, car_obj):
-        self.car = car_obj
-        print("Person got a car")
-
-p1 = Person("Deepak")
-
-print(p1.pname)
-p1.heart.getheart()
-
-c1 = Car("BMW")
-p1.hasCar(c1)
-
-print(p1.car.cname)
-p1.car.getCar()
+class A:
+    def show(self):
+        print("Class A")
+print(A.mro())
+class B:
+    def show(self):
+        print("Class B")
+print(B.mro())
+class C(A,B):
+    def show(self):
+        print("class C")
+print(C.mro())
