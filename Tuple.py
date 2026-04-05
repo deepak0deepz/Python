d9b23a1eba4db4b319112aebbc14d63e146717b2
deepak0deepz <deepakcs2k4@gmail.com>
2026-04-05 12:27:17 +0530

    Updated project files

---
+++ Tuple.py
@@ -0,0 +1,14 @@
+t=(1,2,2,3,4,5,6)
+print(t)
+print(type(t))
+                      #----- Heterogeneous 
+t1=(1,"dcs","012")
+print(t1)
+print(type(t1))
+
+t2=(1)
+t3=("dcs")
+print(type(t2))
+print(type(t3))
+t4=("dcs",)
+print(type(t4))
