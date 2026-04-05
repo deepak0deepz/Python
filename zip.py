b48f8cae8773fd35e9c9250fdc282d334d5bcd17
deepak0deepz <deepakcs2k4@gmail.com>
2026-04-05 13:37:52 +0530

    Updated project files

--- zip.py
+++ zip.py
@@ -1,13 +1,12 @@
-from itertools import zip_longest
 pnames=["kohli","dhoni","iyer","pandya"]
 jnum=[18,7,96,33]
 runs=[8500,5000,3000,6000]
 team=["rcb","csk"]
 
-for i in zip_longest (pnames,jnum,runs,team):
+for i in zip(pnames,jnum,runs,team):
     print(i)
 print()
 
-res=list(zip_longest(pnames,jnum,runs,team))
+res=list(zip(pnames,jnum,runs,team))
 print(res)
 
